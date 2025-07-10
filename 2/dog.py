4c7f79d061336725346d710d7d2b7665b05a487d
Mike Corey <michael.corey@us.af.mil>
2025-07-09 23:01:39 -0500

    example dog class

--- 2/dog.py
+++ 2/dog.py
@@ -4,10 +4,11 @@ class Dog:
         self.age = age
         self.color = color
         self.cute = True
-
     def bark(self, n):
         for _ in range(n):
             print(f'{self.name} says woof.')  
-
     def happy_birthday(self):
         self.age += 1
+    @staticmethod
+    def years_in_human_years(dog_age):
+        return dog_age * 7
